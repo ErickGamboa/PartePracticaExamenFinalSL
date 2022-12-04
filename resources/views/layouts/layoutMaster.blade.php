78f1a6a622b094e4092b4d7ce18f42b5d059f693
erick <erick.yosue@gmail.com>
2022-12-03 22:31:20 -0600

    reparando sitiooo

--- resources/views/layouts/layoutMaster.blade.php
+++ resources/views/layouts/layoutMaster.blade.php
@@ -5,7 +5,3 @@
 $configData = Helper::appClasses();
 @endphp
 
-@isset($configData["layout"])
-@include((( $configData["layout"] === 'horizontal') ? 'layouts.horizontalLayout' :
-(( $configData["layout"] === 'blank') ? 'layouts.blankLayout' : 'layouts.contentNavbarLayout') ))
-@endisset
